67de94802813f0b9fffcfc077174aa5ab0f7fdad
muzhig <homeip127@gmail.com>
2014-08-21 14:23:04 +0600

    Update wsgi_profiler.py

--- wsgi_profiler.py
+++ wsgi_profiler.py
@@ -1,3 +1,6 @@
+# Thanks to @mitsuhiko
+# 90% code is taken from https://github.com/mitsuhiko/werkzeug/blob/master/werkzeug/contrib/profiler.py
+
 import sys
 import time
 import os
@@ -5,23 +8,21 @@ from cProfile import Profile
 from pstats import Stats
 
 
-
 class ProfilerMiddleware(object):
-    def __init__(self, app, stream=None, sort_by=('time', 'calls'), profile_dir=None, filter_func=None, dump_filter_func=None):
+    def __init__(self, app, stream=None, sort_by=('time', 'calls'), profile_dir=None, prefilter=None, postfilter=None):
         self._app = app
         self._stream = stream or sys.stdout
         self._sort_by = sort_by
-        self._restrictions = restrictions
         self._profile_dir = profile_dir
-        self._filter_func = filter_func
-        self._dump_filter_func = dump_filter_func
+        self._prefilter = prefilter
+        self._postfilter = postfilter
         if self._profile_dir and not os.path.isdir(self._profile_dir):
             os.makedirs(self._profile_dir)
 
     def __call__(self, environ, start_response):
         response_body = []
 
-        if self._filter_func and not self._filter_func(environ):
+        if self._prefilter and not self._prefilter(environ):
             return self._app(environ, start_response)
 
         def catching_start_response(status, headers, exc_info=None):
@@ -40,7 +41,7 @@ class ProfilerMiddleware(object):
         body = b''.join(response_body)
         elapsed = time.time() - start
 
-        if not self._dump_filter_func or self._dump_filter_func(environ, body, elapsed):
+        if not self._postfilter or self._postfilter(environ, body, elapsed):
 
             if self._profile_dir is not None:
                 prof_filename = os.path.join(self._profile_dir, '%06dms.%s.prof' % (elapsed * 1000.0, environ.get('PATH_INFO').strip('/').replace('/', '.') or 'root',))
@@ -55,4 +56,3 @@ class ProfilerMiddleware(object):
                 self._stream.write('-' * 80 + '\n\n')
 
         return [body]
-
